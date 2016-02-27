918dc2a5030e062b8e2694e37dafdf158d7bbe9c
kimaiga <nelsonkimaiga@aol.com>
2016-02-27 14:57:53 +0300

    Modified login script, added session initializer

--- src/java/scripts/LoginServlet.java
+++ src/java/scripts/LoginServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import java.sql.ResultSet;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -30,7 +33,7 @@ public class LoginServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        PrintWriter out = response.getWriter();
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -41,7 +44,55 @@ public class LoginServlet extends HttpServlet {
             out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
+            
+            //database connectivity
+            Connection conn= null;
+            String url = "jdbc:mysql://localhost/";
+            String dbName = "leavemanager";
+            String driver ="com.mysql.jdbc.Driver";
+            String username = "root";
+            String password = "";
+            Statement st = null;
+            ResultSet rs;
+            try{
+                Class.forName(driver);
+                conn = DriverManager.getConnection(url+dbName,username,password);
+                st = conn.createStatement();
+            }catch(Exception exp){
+                out.println("<h3>Cannot connect to the database,check network settings.</h3>");
+            }
+            
+    //get request parameters from input fields
+    String user = request.getParameter("username");
+    String pass = request.getParameter("password");
+    
+
+    //form validation
+    if ("".equals(user) || "".equals(pass)){
+        //redirect to error page
+        response.sendRedirect("error.jsp");
+    } else{
+        //attempt to authenticate user
+        try{
+            String sql="SELECT * FROM users WHERE username='"+user+"'and password ='"+pass+"'";
+            rs = st.executeQuery(sql);
+            if (rs.next()) {
+                response.sendRedirect("index.html");
+                    //intiate session
+                    HttpSession session = request.getSession(true);
+                    session.setAttribute("username", user);
+            } 
+            else {
+                response.sendRedirect("google.com");
+                request.getRequestDispatcher("index.html").include(request, response);  
+            }
+        } catch (SQLException x){
+            x.printStackTrace();
         }
+    }   
+            
+            
+            
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
